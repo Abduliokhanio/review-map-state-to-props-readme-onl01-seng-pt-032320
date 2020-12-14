e1996bf5b291ce455299a5ac6f04547f023079d7
Abdul Khan <akhan17@outlook.com>
2020-12-14 21:15:51 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -14,26 +14,28 @@ class App extends Component {
   handleOnClickUsers() {
     this.props.dispatch({
       type: 'GET_COUNT_OF_USERS',
-    });
+    })
   }
 
   render() {
     debugger;
     return (
       <div className="App">
-        <button onClick={() => this.handleOnClickItems()}>
-          Click to change items count
-        </button>
-        <button onClick={() => this.handleOnClickUsers()}>
-          Click to change user count
-        </button>
-        <p> {this.props.items.length}</p>
+          <button onClick={() => this.handleOnClickItems()}>
+            Click to change items count
+            </button>
+          <button onClick={() => this.handleOnClickUsers()}>
+            Click to change user count
+          </button>
+          <p> {this.props.items.length}</p>
       </div>
     );
   }
+}
 
-  const mapStateToProps = (state) => {
-    return { items: state.items }
-  }
+const mapStateToProps = (state) => {
+  // debugger;
+  return { items: state.items }
+}
 
-  export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App);
